dddab89d32bc07f9db80525c3f353ceac6187556
jskutner <jskutner@gmail.com>
2025-06-17 12:58:33 -0400

    added walmart gallery mock

--- app/config/galleries.ts
+++ app/config/galleries.ts
@@ -9,6 +9,10 @@ export const galleries: Record<string, GalleryConfig> = {
     piid: 'f9bf5b29-89fa-45cf-9940-c6962825146f',
     productId: 'high-anxiety',
     scid: '71e2e7cb-eb18-4da9-a37b-6566557c8d1f'
-  }
-  // Add more galleries here as needed
+  },
+  'idc_walmart_gallery_mock': {
+    piid: 'a08d9148-476b-4a21-b803-1c18525e1bca',
+    productId: 'idc_walmart_gallery_mock',
+    scid: '71e2e7cb-eb18-4da9-a37b-6566557c8d1f'
+  }// Add more galleries here as needed
 }; 
